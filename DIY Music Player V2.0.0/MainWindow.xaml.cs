1b4b08a5fe01639d7a6d2c1a95db8e22f3aa302a
Jason Roffel <jasonroffel@hotmail.nl>
2016-06-02 09:56:06 +0200

    Added "empty" project

---
+++ DIY Music Player V2.0.0/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DIY_Music_Player_V2._0._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PlayerUtilities utils;
+        public MainWindow()
+        {
+            utils = new PlayerUtilities(this);
+            InitializeComponent();
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            utils.PlayPause();
+        }
+    }
+}
---
+++ DIY Music Player V2.0.0/PlayerUtilities.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using WMPLib;
+
+namespace DIY_Music_Player_V2._0._0
+{
+    class PlayerUtilities
+    {
+        public PlayerUtilities(Window window)
+        {
+            _window = window;
+            Player.settings.autoStart = false;
+            Player.PlayStateChange += new _WMPOCXEvents_PlayStateChangeEventHandler(player_PlayStateChange);
+        }
+
+        public void GetAllSongsFromDirectory(string directory, SearchOption searchOption)
+        {
+            var fileNames = Directory.GetFiles(directory, ".mp3", searchOption);
+            AllSongs = fileNames.AsQueryable().OrderBy(s => s).ToList();
+        }
+
+        public void PlayPause()
+        {
+
+        }
+
+        public void GetNextSong()
+        {
+
+        }
+
+        private void player_PlayStateChange(int NewState)
+        {
+            if (NewState == (int)WMPPlayState.wmppsMediaEnded)
+            {
+                GetNextSong();
+            }
+        }
+
+        private Window _window;
+        private WindowsMediaPlayer Player = new WindowsMediaPlayer();
+        private List<string> AllSongs = new List<string>();
+    }
+}
